d135bf212235f5b339b2a67861e8e36b7da13f2d
nishadelias <123427128+nishadelias@users.noreply.github.com>
2024-05-09 11:04:12 -0700

    Update arbiter.v

--- arbiter.v
+++ arbiter.v
@@ -50,7 +50,7 @@ counter stopwatch_counter (
     .reset(rst),
     .pause(paused),
     .adjust(adj),
-    .adjust_sel(sel),
+    .select(sel),
     .adjust_clk(clk_2hz),
     .minutes(minutes),
     .seconds(seconds)
@@ -75,7 +75,69 @@ always @(posedge clk_fast) begin
     end else if (pause) begin
         paused <= ~paused;  // Toggle pause state on button press
     end
-  	// call the counter here if pause and rst not pressed
 end
 
 endmodule
+
+
+module counter (
+  input wire clk,
+  input wire reset,
+  input wire pause,
+  input wire adjust,
+  input wire select,
+  input wire adjust_clk,
+  output reg [5:0] minutes,
+  output reg [5:0] seconds
+);
+  
+  initial begin
+    minutes = 0;
+    seconds = 0
+  end
+  
+  always @(posedge clk or posedge reset) begin
+    if (reset) begin
+      seconds <= 0;
+      minutes <= 0;
+    
+    end else if (!pause && !adjust) begin
+      if (seconds < 59) begin
+        seconds <= seconds + 1;
+        
+      end else begin
+        seconds <= 0;
+        if (minutes < 59) begin
+          minutes <= minutes + 1;
+          
+        end else begin
+          minutes <= 0;
+          
+        end
+      end
+    end
+  end
+  
+  
+  always @(posedge adjust_clk) begin
+    if (!reset && adjust) begin
+      if (select) begin
+        if (seconds < 59) begin
+          seconds <= seconds + 1;
+        end else begin
+          seconds <= 0;
+        end
+        
+      end else begin
+        if (minutes < 59) begin
+          minutes <= minutes + 1;
+        end else begin
+          minutes <= 0;
+        end
+      end
+    end
+  end
+  
+  
+endmodule
+
