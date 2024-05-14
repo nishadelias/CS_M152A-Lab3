249f1a5f3722621550318ca50ce27122068568dd
Gregory Payton <107384112+Gpayton787@users.noreply.github.com>
2024-05-14 11:09:19 -0700

    Update clk.v

--- clk.v
+++ clk.v
@@ -1,13 +1,11 @@
 // Code your design here
-module clk_div(input clk input rst, output clk_2hz,output clk_1hz, output clk_fast, output clk_blink)
-
-);
-  input clock_in; // 100 MHz master clock, V10 on board
+module clk_div(input clock_in, // 100 MHz master clock, V10 on board
 output reg clock_out; // output clocks
-output reg clk_2hz;
-output reg clk_1hz;
-output reg clk_fast;
-output reg clk_blink;
+output reg clk_2hz,
+output reg clk_1hz,
+output reg clk_fast,
+output reg clk_blink);
+
 
   reg[27:0] counter_2hz=28'd0;
   reg[27:0] counter_1hz=28'd0;
