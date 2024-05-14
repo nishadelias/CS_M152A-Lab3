7fdc014717b0b9d0fd5c110faa38ae81698cb098
nishadelias <123427128+nishadelias@users.noreply.github.com>
2024-05-14 11:50:21 -0700

    Update tb.v

--- tb.v
+++ tb.v
@@ -3,6 +3,11 @@
 module tb;
 
 reg tb_clk;
+reg tb_clk_2hz;
+reg tb_clk_1hz;
+reg tb_clk_fast;
+reg tb_clk_blink;
+
 reg tb_rst_button;
 reg tb_pause_button;
 reg tb_adj_switch;
@@ -12,21 +17,28 @@ wire [6:0] tb_seg;
 wire tb_dp;
 
 // Create an instance of the arbiter module
-arbiter uut (
-    .clk(tb_clk),
-    .rst_button(tb_rst_button),
-    .pause_button(tb_pause_button),
-    .adj_switch(tb_adj_switch),
-    .sel_switch(tb_sel_switch),
-    .an(tb_an),
-    .seg(tb_seg),
-    .dp(tb_dp)
-);
+//arbiter uut (
+//    .clk(tb_clk),
+//    .rst_button(tb_rst_button),
+//    .pause_button(tb_pause_button),
+//    .adj_switch(tb_adj_switch),
+//    .sel_switch(tb_sel_switch),
+//    .Anode_Activate(tb_an),
+//    .LED_out(tb_seg),
+//);
+
+clk_div uut (
+    .clock_in(tb_clk), // 100 MHz master clock, V10 on board
+    .clk_2hz(tb_clk_2hz),
+    .clk_1hz(tb_clk_1hz),
+    .clk_fast(tb_clk_fast),
+    .clk_blink(tb_clk_blink)
+    );
 
 // clock signal
 always begin
-    tb_clk = 1'b0; #5; // 50MHz clock
-    tb_clk = 1'b1; #5;
+    tb_clk = 1'b0; #10; // 100MHz clock
+    tb_clk = 1'b1; #10;
 end
 
   
@@ -35,4 +47,3 @@ initial begin
     
 end
 
-endmodule
