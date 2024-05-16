eba69b9c8c352b3838a8a14059d37105e937084f
Gregory Payton <107384112+Gpayton787@users.noreply.github.com>
2024-05-15 17:58:57 -0700

    Update clk.v

--- clk.v
+++ clk.v
@@ -1,10 +1,11 @@
 // Code your design here
+// Code your design here
+// Code your design here
 module clk_div(input clock_in, // 100 MHz master clock, V10 on board
-output reg clock_out; // output clocks
-output reg clk_2hz,
-output reg clk_1hz,
-output reg clk_fast,
-output reg clk_blink);
+output reg clk_2hz = 1'b0,
+output reg clk_1hz = 1'b0,
+output reg clk_fast = 1'b0,
+output reg clk_blink = 1'b0);
 
 
   reg[27:0] counter_2hz=28'd0;
