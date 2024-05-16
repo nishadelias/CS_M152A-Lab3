9b7e8b12cf4ccdb43ca2fe4b70340af2b182e36a
nishadelias <123427128+nishadelias@users.noreply.github.com>
2024-05-16 11:24:07 -0700

    Update and rename debounce.v to debouncer.v

---
+++ debouncer.v
@@ -0,0 +1,26 @@
+module debouncer (
+  input clk,
+  input button_in,
+  output button_out
+);
+
+  reg [1:0] count;
+  
+  initial begin
+    count = 0;
+    button_out = 0;
+  end
+
+  always @(*) begin
+    if (button_in != 1 or count == 3) begin
+      count <= 0;
+    end else begin
+      count <= count + 1;
+    end
+    if (count == 3) begin
+      button_out <= 1;
+    end else begin
+      button_out <= 0;
+    end
+  end
+endmodule
