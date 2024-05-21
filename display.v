da119ca7e663e1a2f54e344d1e5fa3ec3469cdd3
Gregory Payton <107384112+Gpayton787@users.noreply.github.com>
2024-05-21 14:45:42 -0700

    Update display.v

--- display.v
+++ display.v
@@ -1,46 +1,28 @@
-module Seven_segment_LED_Display_Controller(
-    input clock_100Mhz, // 100 Mhz clock source on Basys 3 FPGA
-    input reset, // reset
-    output reg [3:0] Anode_Activate, // anode signals of the 7-segment LED display
-    output reg [6:0] LED_out// cathode patterns of the 7-segment LED display
+module display(
+    input clk_fast,
+    input [5:0] minutes,
+    input [5:0] seconds,
+    input adjust,
+    input blink_clk,
+    output reg [3:0] Anode_Activate,
+    output reg [6:0] LED_out
     );
-    reg [26:0] one_second_counter; // counter for generating 1 second clock enable
-    wire one_second_enable;// one second enable for counting numbers
-    reg [15:0] displayed_number; // counting number to be displayed
     reg [3:0] LED_BCD;
     reg [19:0] refresh_counter; // 20-bit for creating 10.5ms refresh period or 380Hz refresh rate
              // the first 2 MSB bits for creating 4 LED-activating signals with 2.6ms digit period
-    wire [1:0] LED_activating_counter; 
+    reg [1:0] LED_activating_counter; 
                  // count     0    ->  1  ->  2  ->  3
               // activates    LED1    LED2   LED3   LED4
              // and repeat
-    always @(posedge clock_100Mhz or posedge reset)
+    initial 
     begin
-        if(reset==1)
-            one_second_counter <= 0;
-        else begin
-            if(one_second_counter>=99999999) 
-                 one_second_counter <= 0;
-            else
-                one_second_counter <= one_second_counter + 1;
-        end
-    end 
-    assign one_second_enable = (one_second_counter==99999999)?1:0;
-    always @(posedge clock_100Mhz or posedge reset)
-    begin
-        if(reset==1)
-            displayed_number <= 0;
-        else if(one_second_enable==1)
-            displayed_number <= displayed_number + 1;
+    LED_activating_counter = 0;
     end
-    always @(posedge clock_100Mhz or posedge reset)
-    begin 
-        if(reset==1)
-            refresh_counter <= 0;
-        else
-            refresh_counter <= refresh_counter + 1;
-    end 
-    assign LED_activating_counter = refresh_counter[19:18];
+  	
+    always @(posedge clk_fast) begin
+        LED_activating_counter <= LED_activating_counter + 1;
+    end
+
     // anode activating signals for 4 LEDs, digit period of 2.6ms
     // decoder to generate anode signals 
     always @(*)
@@ -49,25 +31,25 @@ module Seven_segment_LED_Display_Controller(
         2'b00: begin
             Anode_Activate = 4'b0111; 
             // activate LED1 and Deactivate LED2, LED3, LED4
-            LED_BCD = displayed_number/1000;
+            LED_BCD = minutes/10;
             // the first digit of the 16-bit number
               end
         2'b01: begin
             Anode_Activate = 4'b1011; 
             // activate LED2 and Deactivate LED1, LED3, LED4
-            LED_BCD = (displayed_number % 1000)/100;
+            LED_BCD = minutes%10;
             // the second digit of the 16-bit number
               end
         2'b10: begin
             Anode_Activate = 4'b1101; 
             // activate LED3 and Deactivate LED2, LED1, LED4
-            LED_BCD = ((displayed_number % 1000)%100)/10;
+            LED_BCD = seconds/10;
             // the third digit of the 16-bit number
                 end
         2'b11: begin
             Anode_Activate = 4'b1110; 
             // activate LED4 and Deactivate LED2, LED3, LED1
-            LED_BCD = ((displayed_number % 1000)%100)%10;
+            LED_BCD = seconds%10;
             // the fourth digit of the 16-bit number    
                end
         endcase
@@ -75,6 +57,8 @@ module Seven_segment_LED_Display_Controller(
     // Cathode patterns of the 7-segment LED display 
     always @(*)
     begin
+      if(blink_clk) LED_out = 7'b1111111;
+      else begin
         case(LED_BCD)
         4'b0000: LED_out = 7'b0000001; // "0"     
         4'b0001: LED_out = 7'b1001111; // "1" 
@@ -88,5 +72,6 @@ module Seven_segment_LED_Display_Controller(
         4'b1001: LED_out = 7'b0000100; // "9" 
         default: LED_out = 7'b0000001; // "0"
         endcase
+      end
     end
  endmodule
