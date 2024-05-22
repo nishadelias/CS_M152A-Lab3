f0b0dfa56a456718018e91fcdfcdc215161ee9f1
nishadelias <123427128+nishadelias@users.noreply.github.com>
2024-05-22 16:21:19 -0700

    formatted it better

--- arbiter.v
+++ arbiter.v
@@ -287,52 +287,22 @@ module display(
                 endcase
               end
             end else begin
-                            case(LED_BCD)
-                            4'b0000: LED_out = 7'b0000001; // "0"     
-                            4'b0001: LED_out = 7'b1001111; // "1" 
-                            4'b0010: LED_out = 7'b0010010; // "2" 
-                            4'b0011: LED_out = 7'b0000110; // "3" 
-                            4'b0100: LED_out = 7'b1001100; // "4" 
-                            4'b0101: LED_out = 7'b0100100; // "5" 
-                            4'b0110: LED_out = 7'b0100000; // "6" 
-                            4'b0111: LED_out = 7'b0001111; // "7" 
-                            4'b1000: LED_out = 7'b0000000; // "8"     
-                            4'b1001: LED_out = 7'b0000100; // "9" 
-                            default: LED_out = 7'b0000001; // "0"
-                            endcase
-                          end
+                case(LED_BCD)
+                4'b0000: LED_out = 7'b0000001; // "0"     
+                4'b0001: LED_out = 7'b1001111; // "1" 
+                4'b0010: LED_out = 7'b0010010; // "2" 
+                4'b0011: LED_out = 7'b0000110; // "3" 
+                4'b0100: LED_out = 7'b1001100; // "4" 
+                4'b0101: LED_out = 7'b0100100; // "5" 
+                4'b0110: LED_out = 7'b0100000; // "6" 
+                4'b0111: LED_out = 7'b0001111; // "7" 
+                4'b1000: LED_out = 7'b0000000; // "8"     
+                4'b1001: LED_out = 7'b0000100; // "9" 
+                default: LED_out = 7'b0000001; // "0"
+                endcase
+            end
     end
-    // Cathode patterns of the 7-segment LED display 
-//    always @(*)
-//    begin
-//        if(blink_clk && adjust)begin
-//        if(select && ( Anode_Activate == 4'b0111 || Anode_Activate == 4'b1011)) 
-//            begin
-//             LED_out = 7'b1111111;
-//            end
-//        else if(!select && (Anode_Activate == 4'b1101 || Anode_Activate == 4'b1110))
-//            begin
-//                LED_out = 7'b1111111;
-//            end
-//       end
-   
-        
-//      else begin
-//        case(LED_BCD)
-//        4'b0000: LED_out = 7'b0000001; // "0"     
-//        4'b0001: LED_out = 7'b1001111; // "1" 
-//        4'b0010: LED_out = 7'b0010010; // "2" 
-//        4'b0011: LED_out = 7'b0000110; // "3" 
-//        4'b0100: LED_out = 7'b1001100; // "4" 
-//        4'b0101: LED_out = 7'b0100100; // "5" 
-//        4'b0110: LED_out = 7'b0100000; // "6" 
-//        4'b0111: LED_out = 7'b0001111; // "7" 
-//        4'b1000: LED_out = 7'b0000000; // "8"     
-//        4'b1001: LED_out = 7'b0000100; // "9" 
-//        default: LED_out = 7'b0000001; // "0"
-//        endcase
-//      end
-//    end
+
  endmodule
  
  module debouncer (
